5798390d8db0ed93828b1b326cf5d6bacc27d997
Raj Ankathi <rjankathi@gmail.com>
2019-09-20 16:44:05 -0700

    feat(random): pass a number to the random functin,to  get an 'n' array of movies
    closes #2
    
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 var uniqueRandomArray = require('unique-random-array');
 var movies = require('./movies.json');
+var getRandomItem = uniqueRandomArray(movies);
+
 module.exports = {
     all: movies,
-    random: uniqueRandomArray(movies),
-    getMovieByName: function (title){
-        var movie =  movies.filter((item) => { return title.includes(item.title);});
+    random: random,
+    movieByName: movieByName
+};
+
+function movieByName(title){
+    var movie =  movies.filter((item) => { return title.includes(item.title);});
         if(movie.length == 0 || movie === undefined){
             return `The Movie youre looking for is not found`;
         } else{
             return movie;
         }
-    },
-};
+}
 
+function random(number){
+    if(number === undefined){
+    return getRandomItem();
+    } else {
+        var randomItems = [];
+        for(var i=0; i < number ; i++){
+            randomItems.push(getRandomItem());
+        }
+        return randomItems;
+    }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -6,52 +6,55 @@ describe('movies-names',function(){
         it('should be an array of objects',function(){
             expect(movies.all).to.satisfy(isArrayOfObjects);
 
-            function isArrayOfObjects(array){
-                return array.every(function(item){
-                    return typeof item === 'object';
-                });
-            }
-        });
-
         it('should get movie by title',function(){
-            var movie = movies.getMovieByName('Gandhi');
+            var movie = movies.movieByName('Gandhi');
             expect(movie[0].title).to.equal('Gandhi');
         });
 
-        describe('all',function(){
-            it('should not be `0` in length or undefined',function(){
-                expect(movies.all).to.not.equal(0 || undefined);
+        it('should not be `0` in length or undefined',function(){
+            expect(movies.all).to.not.equal(0 || undefined);
+        });
+        function isArrayOfObjects(array){
+            return array.every(function(item){
+                return typeof item === 'object';
+            });
+        }
+        });
+
+    }); 
+
+    describe('random',function(){
+        it('should not be null or empty',function(){
+            expect(movies.random()).to.not.equal({});
+        });
+
+        it('should return a random item from movies.all',function (){
+            var randomItem = movies.random();
+            expect(movies.all).to.include(randomItem);
+        });
+
+        it('should return an array of random objects if passed a number',function(){
+            var randomItems = movies.random(3);
+            expect(randomItems).to.have.length(3);
+            randomItems.forEach((item) => {
+                expect(movies.all).to.include(item);
             });
         });
 
-        describe('random',function(){
-            it('should not be null or empty',function(){
-                expect(movies.random()).to.not.equal({});
-            });
+    });
+
+    describe('movieByName()',function(){
+        it('should log `The Movie youre looking for is not found` if movie is not found',function(){
+            var movieNotInTheArray = movies.movieByName('');
+            console.log();
+            expect(movies.movieByName('')).to.equal(movieNotInTheArray);
         });
 
-
-        describe('random',function(){
-            it('should return a random item from movies.all',function (){
-                var randomItem = movies.random();
-                expect(movies.all).to.include(randomItem);
-            });
-        });
-
-        describe('getMovieByName()',function(){
-            it('should log `The Movie youre looking for is not found` if movie is not found',function(){
-                var movieNotInTheArray = movies.getMovieByName('');
-                console.log();
-                expect(movies.getMovieByName('')).to.equal(movieNotInTheArray);
-            });
-        });
-
-        describe('getMovieByName(`Gandhi`)',function(){
-            it('should not be empty',function(){
-                var movieNotInTheArray = movies.getMovieByName('Gandhi');
-                console.log();
-                expect(movies.getMovieByName('Gandhi')).to.not.equal({});
-            });
+        it('should not be empty',function(){
+            var movieNotInTheArray = movies.movieByName('Gandhi');
+            console.log();
+            expect(movies.movieByName('Gandhi')).to.not.equal({});
         });
     });
+
 });
